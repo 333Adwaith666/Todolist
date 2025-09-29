01c1b7bdfffe888a96ff9529e8467740670cdc12
Adwaith santhosh <adwaithsanthosh971@gmail.com>
2025-09-29 22:31:46 +0530

    Delete ToDoList directory

--- ToDoList/src/main/resources/static/js/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('form[action*="/delete"]').forEach(form => {
-        form.addEventListener('submit', e => {
-            if(!confirm('Are you sure?')) e.preventDefault();
-        });
-    });
-});
